3af365da2ecab64fddf21a028828949fd8ff2cff
N9verMore <ilya.tsyunyak@gmail.com>
2023-11-28 13:21:46 +0200

    DIA changes. Static branch_id

--- src/main/java/com/sachuk/keu/controllers/rest/dia/DiaAuthController.java
+++ src/main/java/com/sachuk/keu/controllers/rest/dia/DiaAuthController.java
@@ -1,7 +1,6 @@
 package com.sachuk.keu.controllers.rest.dia;
 
 import com.sachuk.keu.services.dia.DiaAuthService;
-import com.sachuk.keu.services.dia.DiaDocumentsService;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +23,8 @@ public class DiaAuthController {
     public String authorize(Model model) {
         try {
             String session_token = service.getCreateSessionAndGetToken();
-            String branch_id = service.postCreateBranchAndGetId(session_token);
+            // String branch_id = service.postCreateBranchAndGetId(session_token);
+            String branch_id = "4caf73a1a770b0ca18a9d1aa454d627954c6477097ce95aca4893211bf5bbd7c5bb7eac945670aed9af612245cbd39bc82c2ad725433b3f0794a535ccb7b3c65";
             String offer_id = service.postCreateOfferAndGetId(session_token, branch_id, this.returnLink);
             String deepLink = service.postCreateOfferRequestAndGetDeeplink(branch_id, offer_id, this.returnLink, session_token);
             model.addAttribute("deeplink", deepLink);
@@ -34,5 +34,6 @@ public class DiaAuthController {
             return "error"; // TODO Redirect where?
         }
     }
+
 
 }
