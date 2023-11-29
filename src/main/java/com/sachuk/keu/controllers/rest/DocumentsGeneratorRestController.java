7075d8932f4657bba543f3e3e9e5ac3659cefb3f
Arthur <114497658+DexSus@users.noreply.github.com>
2023-11-29 11:52:01 +0200

    Fix DocumentGeneratorService and DocumentsGeneratorRestController

--- src/main/java/com/sachuk/keu/controllers/rest/DocumentsGeneratorRestController.java
+++ src/main/java/com/sachuk/keu/controllers/rest/DocumentsGeneratorRestController.java
@@ -5,13 +5,16 @@ import com.sachuk.keu.database.service.MilitaryManService;
 import com.sachuk.keu.entities.MilitaryMan;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,7 +29,7 @@ public class DocumentsGeneratorRestController {
     public final DocumentGeneratorService generatorService;
 
     @GetMapping("/downloadExhaustFromKEU/{id}")
-    public ResponseEntity<ByteArrayResource> download(@PathVariable("id") Long milManId){
+    public ResponseEntity<InputStreamResource> download(@PathVariable("id") Long milManId){
         MilitaryMan militaryMan;
         if (militaryManService.findById(milManId).isPresent()) {
             militaryMan = militaryManService.findById(milManId).get();
@@ -35,15 +38,12 @@ public class DocumentsGeneratorRestController {
             File file = new File(pathToGeneratedFile);
 
             HttpHeaders header = new HttpHeaders();
-            header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Витяг_з_КЕУ_" + militaryMan.getSurname() + ".docx");
-            header.add("Cache-Control", "no-cache, no-store, must-revalidate");
-            header.add("Pragma", "no-cache");
-            header.add("Expires", "0");
+            header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
 
             Path path = Paths.get(file.getAbsolutePath());
-            ByteArrayResource resource = null;
+            InputStreamResource resource = null;
             try {
-                resource = new ByteArrayResource(Files.readAllBytes(path));
+                resource = new InputStreamResource(Files.newInputStream(file.toPath()));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
